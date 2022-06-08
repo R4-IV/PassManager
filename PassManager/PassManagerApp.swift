5f5228da9d768dde3ef59bc20d8736d813099f2b
M00738240 <M00738240@live.mdx.ac.uk>
2022-06-08 16:54:56 +0100

    Initial Commit

---
+++ PassManager/PassManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassManagerApp.swift
+//  PassManager
+//
+//  Created by R4 on 08/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct PassManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
